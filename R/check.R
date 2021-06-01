4015aa1dd4f506ba341ceb8c112640fd9bce0f4b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-01 14:46:50 +0100

    Correct name

--- R/check.R
+++ R/check.R
@@ -3,7 +3,7 @@
 ##' @title Check a model type
 ##' @param model_type String
 ##' @export
-spimalot_check_model_type <- function(model_type) {
+spim_check_model_type <- function(model_type) {
   if (!(model_type %in% c("BB", "NB"))) {
     stop(sprintf("Expected '%s' to be one of 'BB' or 'NB'", model_type),
          call. = FALSE)
