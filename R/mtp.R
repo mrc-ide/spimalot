6835068625984fc148f707b97025ec91327cf97f
pabloperguz <p.perez-guzman@imperial.ac.uk>
2021-06-15 18:08:03 +0100

    export tested mtp support functions

--- R/mtp.R
+++ R/mtp.R
@@ -1,11 +1,87 @@
-spim_mtp_prepare <- function(mtp_commission, npi_key, n_par, end_date,
-                             vaccine_parameters, combined, rt) {
-  date <- combined$date
-  npi_key <- mtp_npi_key_prepare(npi_key, rt, date)
-  ## If date is NULL we get NaN entries for Rt and an obscure error
-  ## later.
+##' Prepare current Rt values for MTP simulations
+##'
+##' @title Prepare Rt for MTP scenarios
+##'
+##' @param rt Outputs for Rt by region from rtm_inference_spim_fits2_combined
+##'
+##' @param type A string or vector of Rt type to use, can be one of
+##'   "eff_Rt_general", "Rt_general", "eff_Rt_all" or "Rt_all".
+##'
+##'
+##' @export
+spim_mtp_rt <- function(rt, date, type = c("eff_Rt_general", "Rt_general")){
+  ret <- NULL
+  regions <- c(sircovid::regions("all"), "england", "uk")
 
-  ## TODO: might move 'rt' into the combined depending on size?
+  for (t in type) {
+    for (r in regions) {
+      ret[[t]][[r]] <-
+        rt[[r]][[t]][rt[[r]]$date == sircovid::sircovid_date(date)]
+    }
+  }
+  ret
+}
+
+##' Prepare NPIs effect on Rt for MTP simulations
+##'
+##' @title Prepare NPIs for MTP scenarios
+##' @param npi_key A CSV file containing NPI parameters
+##'
+##' @param Rt Rt values for all regions and nations obtained via
+##'   [spimalot::spim_mtp_rt]
+##'
+##' @param rt_reduction A double specifying a reduction on Rt (excluding
+##'   immunity) given school closures. A default value of 0.5 roughly yields
+##'   a 0.3 reduction in eff_Rt.
+##'
+##'
+##' @export
+spim_mtp_npi_key <- function(npi_key, Rt, rt_reduction = 0.5){
+
+  curr_Rt <- Rt$Rt_general
+  curr_eff_Rt <- Rt$eff_Rt_general
+
+  npi_key$Rt <- round(npi_key$Reff_t * mean(curr_Rt[["uk"]] /
+                                              curr_eff_Rt[["uk"]]), 1)
+
+  npi_key["schools_main", "Rt"] <- round(mean(curr_Rt[["uk"]]), 1)
+
+  npi_key_holidays <- npi_key
+  npi_key_holidays$Rt <- npi_key$Rt - rt_reduction
+  rownames(npi_key_holidays) <- gsub("schools", "holidays", rownames(npi_key))
+
+  npi_key <- rbind(npi_key, npi_key_holidays)
+}
+
+##' Prepare scenario simulations for MTP commission
+##'
+##' @title Prepare MTP scenarios
+##' @param mtp_commission A CSV specifying MTP scenarios for SPI-M
+##'
+##' @param npi_key A CSV with corresponding effect of NPIs on Rt values
+##'
+##' @param end_date End date for the simulation, this should usually be
+##'   current date + 9 weeks
+##'
+##' @param vaccine_parameters Output from upstream task rtm_vaccination_parameters
+##'
+##' @param combined Output from combined fitting task rtm_inference_pmcmc_spim_fits2_combined
+##'
+##' @param rt Rt values for all regions and nations obtained via
+##'   [spimalot::spim_mtp_rt]
+##'
+##'
+##' @export
+##'
+spim_mtp_prepare <- function(mtp_commission, combined,
+                             vaccination_parameters, npi_key, rt) {
+  date <- combined$date
+
+  ## get current Rt for all regions and nations
+  curr_Rt <- spim_mtp_rt(rt, date)
+
+  ## get NPIs key for simulation
+  npi_key_expand <- spim_mtp_npi_key(npi_key, curr_Rt)
 
   ## expand data frame to include all regions
   f <- function(df) {
@@ -25,20 +101,29 @@ spim_mtp_prepare <- function(mtp_commission, npi_key, n_par, end_date,
   ret <- lapply(mtp_commission, f) %>%
     dplyr::bind_rows()
 
+
   run_grid <- ret %>%
-    dplyr::select(.data$scenario, .data$daily_doses, .data$spim_name,
-                  .data$type_rt) %>%
+    dplyr::select(scenario, daily_doses, spim_name, type_rt) %>%
     dplyr::distinct()
 
   rt_future <- ret %>%
-    dplyr::select(-c(.data$daily_doses, .data$spim_name)) %>%
-    tidyr::pivot_longer(c(tidyr::starts_with("npi"),
-                          tidyr::starts_with("date")),
+    dplyr::select(-c(daily_doses, spim_name)) %>%
+    tidyr::pivot_longer(c(starts_with("npi"), starts_with("date")),
                         names_to = c(".value", "changepoint"),
                         names_pattern = "(.+)_(.+)") %>%
-    dplyr::mutate(Rt_sd = npi_key[npi, "Rt_sd"],
-                  Rt = npi_key[npi, "Rt"],
+    dplyr::mutate(Rt_sd = npi_key_expand[npi, "Rt_sd"],
+                  Rt = npi_key_expand[npi, "Rt"],
                   .after = npi)
+
+  for (r in unique(rt_future$region)){
+    pivot <- rt_future$pivot[rt_future$region == r]
+    if (pivot == "scaling"){
+      type_rt <- rt_future$type_rt[rt_future$region == r]
+      base_Rt <- mean(curr_Rt[[type_rt]][[r]])
+      rt_future$Rt[rt_future$region == r] = base_Rt +
+        rt_future$Rt_1[rt_future$region == r]
+    }
+  }
 
   rt_future <- split(rt_future, f = rt_future$scenario)
   rt_future <- rt_future[run_grid$scenario]
@@ -50,43 +135,173 @@ spim_mtp_prepare <- function(mtp_commission, npi_key, n_par, end_date,
                  prevalence = prev_states,
                  keep = c(inc_states, prev_states))
 
-  vaccine <- list(
-    future_daily_doses = lapply(vaccine_parameters$daily_doses, "[[", "mtp"),
-    efficacy = vaccine_parameters$vacc_efficacy$central,
-    uptake = vaccine_parameters$uptake_by_age$central)
+  ## Vaccination parameters we need
+  future_daily_doses <- lapply(vaccination_parameters$daily_doses, "[[", "mtp")
+  vaccine_efficacy <- vaccination_parameters$vacc_efficacy$central
+  vaccine_uptake <- vaccination_parameters$uptake_by_age$central
+  eligibility_by_age <- vaccination_parameters$eligibility_by_age$min_18
 
-  n_threads <- spim_control_cores()
-  message(sprintf("Running on %d threads", n_threads))
-
-  list(length = nrow(run_grid),
-       combined = combined,
-       n_threads = n_threads,
-       n_par = n_par,
-       end_date = end_date,
+  list(curr_Rt = curr_Rt,
+       npi_key = npi_key_expand,
        run_grid = run_grid,
        rt_future = rt_future,
-       vaccine = vaccine,
+       future_daily_doses = future_daily_doses,
+       vaccine_efficacy = vaccine_efficacy,
+       vaccine_uptake = vaccine_uptake,
+       eligibility_by_age = eligibility_by_age,
        output = output)
 }
 
 
-mtp_npi_key_prepare <- function(npi_key, rt, date) {
-  curr_eff_Rt <- rt$eff_Rt_general[rt$date == sircovid::sircovid_date(date)]
-  curr_Rt <- rt$Rt_general[rt$date == sircovid::sircovid_date(date)]
-  npi_key$Rt <- round(npi_key$Reff_t * mean(curr_Rt / curr_eff_Rt), 1)
-  npi_key["schools_main", "Rt"] <- round(mean(curr_Rt), 1)
+summary_to_template <- function(summary_tidy, run_grid, pop, template_common,
+                                date, regions, spim_state_names, model) {
 
-  ## translate 0.5 reduction in Rt_excl_immunity for school closures
-  ## into equivalent reduction in eff_Rt, this is likely to be around
-  ## 0.3
+  ## create common template columns
+  lapply(run_grid$spim_name, template_common,
+         date = date, model = model) %>%
+    dplyr::bind_rows() %>%
+    ## join to results
+    dplyr::left_join(summary_tidy$state, by = c(Scenario = "spim_name")) %>%
+    dplyr::filter(state %in% names(spim_state_names),
+                  date >= !!date,
+                  group == "all") %>%
+    dplyr::mutate("Day of Value" = lubridate::day(date),
+                  "Month of Value" = lubridate::month(date),
+                  "Year of Value" = lubridate::year(date),
+                  AgeBand = "All",
+                  Geography = regions[as.character(region), "name"],
+                  ValueType = spim_state_names[as.character(state)],
+                  quantile = as.numeric(gsub("%", "", quantile)) / 100,
+                  value = if_else(state == "react_pos",
+                                  value / pop[as.character(region)] * 100,
+                                  value),
+                  .after = "Creation Year") %>%
+    dplyr::select(-c(scenario, daily_doses, type_rt, date, state, region, group,
+                     vaccine_status)) %>%
+    tidyr::pivot_wider(names_from = quantile, values_from = value,
+                       names_prefix = "Quantile ") %>%
+    dplyr::mutate(Value = `Quantile 0.5`, .after = ValueType)
 
-  npi_key_holidays <- npi_key
-  npi_key_holidays$Rt <- npi_key$Rt - 0.5
-  rownames(npi_key_holidays) <- gsub("schools", "holidays", rownames(npi_key))
-
-  rbind(npi_key, npi_key_holidays)
 }
 
+save_results <- function(dat, path_template, path_save, root = "outputs") {
+
+  sheets <- readxl::excel_sheets(path_template)
+  template <- setNames(
+    lapply(sheets, readxl::read_xlsx, path = path_template,
+           .name_repair = "minimal"),
+    sheets)
+  stopifnot(identical(names(template[[2]]), names(dat)))
+  template[[2]] <- dat
+  writexl::write_xlsx(template, sprintf("%s/%s", root, path_save))
+}
+
+template_common <- function(date, scenario, model) {
+
+  version <- packageVersion("sircovid")
+  version <- substr(version, 3, nchar(version))
+
+  data.frame(Group = "Imperial",
+             Model = model,
+             Scenario = scenario,
+             ModelType = "Multiple",
+             Version = version,
+             "Creation Day" = lubridate::day(date),
+             "Creation Month" = lubridate::month(date),
+             "Creation Year" = lubridate::year(date),
+             stringsAsFactors = FALSE,
+             check.names = FALSE)
+}
+
+
+get_age_vaccine_outputs <- function(res, region) {
+
+  ## Objects for saving list of plots and matrices
+  scenario_plots <- NULL
+  scenario_plots_proportion <- NULL
+  scenario_matrices <- NULL
+
+  res <- dplyr::filter(res, region == !!region,
+                       group != "all",
+                       state %in% c("infections_inc", "diagnoses_admitted_inc",
+                                    "deaths_inc", "deaths"))
+
+  for (s in unique(res$scenario)) {
+
+    tmp <- dplyr::filter(res, scenario == s)
+    plots_age_vacc <- NULL
+    plots_age_vacc_prop <- NULL
+
+
+    for (w in unique(res$state)) {
+      ## Vector of dates for plotting
+
+      plot_matrix <- tmp %>%
+        dplyr::filter(state == w)  %>%
+        dplyr::mutate(age = factor(group,
+                                   levels = unique(group),
+                                   labels = c("Under 30s", "30 to 49", "50 to 74",
+                                              "75+")))
+      plots_age_vacc[[w]] <- ggplot(plot_matrix, aes(date, value, fill = age)) +
+        ylab(paste(stringr::str_to_sentence(w))) + xlab("") +
+        geom_area() + theme_bw() + facet_wrap(vars(vaccine_status)) +
+        ggsci::scale_fill_lancet() +
+        theme(axis.title.y = element_text(size = rel(0.9)),
+              legend.position = "none",
+              axis.title.x = element_text(size = rel(0.8)),
+              legend.title = element_blank(),
+              strip.text.x = element_text(size = rel(0.7)))
+
+      plots_age_vacc_prop[[w]] <-
+        ## Re-arrange matrix for proportional stacked chart
+        plot_matrix %>%
+        group_by(date, vaccine_status, age) %>%
+        summarise(n = sum(value)) %>%
+        mutate(percentage = n / sum(n)) %>%
+        # mutate(percentage = if_else(sum(n) == 0, 0, n / sum(n))) %>%
+        ## Plot
+        ggplot(., aes(x=date, y=percentage, fill=age)) +
+        geom_area(alpha=0.6 , size=1, colour="black") +
+        ylab("") + xlab("") +
+        geom_area() + theme_bw() + facet_wrap(vars(vaccine_status)) +
+        ggsci::scale_fill_lancet() +
+        theme(axis.title.y = element_text(size = rel(0.9)),
+              axis.title.x = element_text(size = rel(0.8)),
+              legend.title = element_blank(),
+              strip.text.x = element_text(size = rel(0.7)))
+
+      if (w != "diagnoses_admitted_inc"){
+        plots_age_vacc_prop[[w]] <- plots_age_vacc_prop[[w]] +
+          theme(legend.position = "none")}
+
+      scenario_matrices[[s]][[w]] <- plot_matrix
+    }
+
+    ## Save plot and matrix object into lists
+
+    scenario_plots[[s]] <-
+      (plots_age_vacc[["infections_inc"]] + plots_age_vacc_prop[["infections_inc"]]) /
+      (plots_age_vacc[[ "diagnoses_admitted_inc"]] + plots_age_vacc_prop[[ "diagnoses_admitted_inc"]]) /
+      (plots_age_vacc[["deaths_inc"]] + plots_age_vacc_prop[["deaths_inc"]]) /
+      (plots_age_vacc[["deaths"]] + plots_age_vacc_prop[["deaths"]]) +
+      plot_annotation(
+        caption = "Partial immunity includes those with either one or two doses that have not yet achieved full vaccine efficacy",
+        title = paste(stringr::str_to_sentence(region)),
+        subtitle = paste(stringr::str_to_sentence(sub("_", " ", s))))
+
+  }
+
+  ## Main scenario always first element in list
+  main_scenario <- grep("main", names(scenario_matrices))
+  other_scenarios <- grep("main", names(scenario_matrices), invert = TRUE)
+  scenario_plots <- scenario_plots[c(main_scenario, other_scenarios)]
+  scenario_matrices <- scenario_matrices[c(main_scenario, other_scenarios)]
+
+  out <- NULL
+  out[["plots"]] <- scenario_plots
+  out[["matrices"]] <- scenario_matrices
+  out
+}
 
 spim_mtp_simulate <- function(obj, subset = NULL) {
   n_run <- obj$length
