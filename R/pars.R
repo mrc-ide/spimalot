bfea8b4775e63d001af7dab9e2de3cdede1c2743
edknock <e.knock@imperial.ac.uk>
2021-06-21 11:10:49 +0100

    add beta19

--- R/pars.R
+++ R/pars.R
@@ -159,14 +159,14 @@ spim_pars_beta <- function(date) {
   ## 16. 2021-03-08 - Step 1 of roadmap: schools reopen
   ## 17. 2021-04-19 - Step 2 of roadmap: outdoors hospitality (04-12)
   ##                  and shools return (04-19)
-  ## 18. Two weeks ago
-  ## TODO: recent dates to consider: schools return (2021-04-19)
+  ## 18. 2021-05-17 - Step 3 of roadmap: indoors hospitality
+  ## 18. Three weeks ago
   c("2020-03-16", "2020-03-23", "2020-03-25",
     "2020-05-11", "2020-06-15", "2020-07-04",
     "2020-08-01", "2020-09-01", "2020-09-14",
     "2020-10-14", "2020-10-31", "2020-11-05",
     "2020-12-02", "2020-12-18", "2021-01-05",
-    "2021-03-08", "2021-04-19",
+    "2021-03-08", "2021-04-19", "2021-05-17",
     as.character(as.Date(date) - 21))
 }
 
