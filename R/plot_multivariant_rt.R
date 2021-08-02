5df8314081d2557637ea6d446e5ca9aa26356ef0
edknock <47318334+edknock@users.noreply.github.com>
2021-08-02 17:09:58 +0100

    Update R/plot_multivariant_rt.R

--- R/plot_multivariant_rt.R
+++ R/plot_multivariant_rt.R
@@ -35,7 +35,7 @@ spim_multivariant_rt_plot <- function(dat, date, date_restart,
       "Roadmap Step 2",
       "Roadmap Step 3",
       "Original planned date of Step 4",
-      "Euros 2021 Final")
+      "Euro 2020 Final")
   ) %>% dplyr::filter(!stringr::str_detect(label, "School"))
 
   rt_plot <- NULL
