809f7ed8e081ad64361fa3d0a8c74065cd50a589
Raphael Sonabend <raphaelsonabend@gmail.com>
2021-08-26 10:39:02 +0100

    remove round_to

--- R/tools.R
+++ R/tools.R
@@ -78,19 +78,3 @@ spim_rrq_controller <- function(root = here::here()) {
     }
   }
 }
-
-
-##' Round to nearest given number
-##' @title Round to given number
-##' @param x Number to round
-##' @param y Number to round to
-##' @export
-##' @examples
-##'  # Round to nearest 100
-##'  spim_round_to(146, 100)
-##'  # Round to nearest 1000
-##'  spim_round_to(7849, 1000)
-spim_round_to <- function(x, y) {
-  stopifnot(y >= 1)
-  round(x / y) * y
-}
