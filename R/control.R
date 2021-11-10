b2cb767828cc164db086ab0fe447143ba1e0a85a
Marc Baguelin <mbagueli@wpia-dide280.dide.ic.ac.uk>
2021-11-10 12:23:14 +0000

    Remove restart for the deterministic filter as it is not supported at the moment by mcstate

--- R/control.R
+++ R/control.R
@@ -78,6 +78,9 @@ spim_control <- function(short_run, n_chains, deterministic = FALSE, date_restar
     ## Disable early exit, if it's been set up, as we also don't support that
     pmcmc$filter_early_exit <- FALSE
 
+    ## Disable restart, as the deterministic fit does not support this yet
+    control$pmcmc$save_restart <- NULL
+
     ## Increase the number of workers because each will be running
     ##   separately. If running on a laptop this probably does not want
     ##   increasing
