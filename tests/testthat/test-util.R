6fbbc5e61a5adceae5b037dcd95f0781f75029ad
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-12-09 16:09:59 +0000

    Add support for immutable objects

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -10,3 +10,25 @@ test_that("data_frame does not mangle names", {
   expect_equal(names(data_frame("name with spaces" = 1:10, b = 1)),
                c("name with spaces", "b"))
 })
+
+
+test_that("immutable objects cannot be assigned", {
+  x <- list(a = 1, b = 2)
+  class(x) <- c("myobj", "immutable")
+
+  expect_error(
+    x$a <- 10,
+    "Objects of class 'myobj' are immutable and you may not alter them")
+  expect_error(
+    x[["a"]] <- 10,
+    "Objects of class 'myobj' are immutable and you may not alter them")
+  expect_error(
+    x[[1]] <- 10,
+    "Objects of class 'myobj' are immutable and you may not alter them")
+  expect_error(
+    x[c("b", "d")] <- list(10, 20),
+    "Objects of class 'myobj' are immutable and you may not alter them")
+  expect_error(
+    x[1:2] <- list(10, 20),
+    "Objects of class 'myobj' are immutable and you may not alter them")
+})
