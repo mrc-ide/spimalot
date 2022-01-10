90b811691522aff2ea7bad41734c8f49fb34bf54
Divyakanapram <dthekkek@ic.ac.uk>
2022-01-10 15:03:07 +0000

    Added comments on new beta date (2021-12-23).

--- R/pars.R
+++ R/pars.R
@@ -190,7 +190,8 @@ spim_pars_beta <- function(date, last_beta_days_ago = 21) {
   ## 29. 2021-10-22 - School half-term - Point before recent plateau in cases
   ## 30. 2021-11-01 - Schools return
   ## 31. 2021-12-08 - Announcement of move to Plan B
-  ## 32. Date - last_beta_days_ago
+  ## 32. 2021-12-23 - Starting of X-mas holidays.
+  ## 33. Date - last_beta_days_ago
   c("2020-03-16", "2020-03-23", "2020-03-25",
     "2020-05-11", "2020-06-15", "2020-07-04",
     "2020-08-01", "2020-09-01", "2020-09-14",
