c28a995b26ee03f40280918f898dca300cd76521
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-01-14 12:09:16 +0000

    Add new utility

--- R/check.R
+++ R/check.R
@@ -28,3 +28,35 @@ spim_check_sircovid_model <- function(sircovid_model) {
 check_region <- function(region) {
   match_value(region, sircovid::regions("all"))
 }
+
+##' Validate (and possibly expand) a region for a fit.
+##'
+##' @title Validate and expand regions
+##'
+##' @param region A single string indicating a region or group of
+##'   regions.  For `multiregion = FALSE` this must be a suitable NHS
+##'   region.  When `multiregion = TRUE` this must be one of `all`,
+##'   `england` or `test`
+##'
+##' @param multiregion Logical, indicating if this is a multiregion fit
+##'
+##' @return A character vector of regions
+##' @export
+spim_check_region <- function(region, multiregion) {
+  assert_scalar_character(region)
+  if (multiregion) {
+    if (region %in% c("all", "england")) {
+      region <- sircovid::regions(region)
+    } else if (region == "test") {
+      ## We could use a different set here really, or something more
+      ## clever
+      region <- c("london", "south_west")
+    } else {
+      stop(sprintf("Invalid value '%s' for 'region' when multiregion = TRUE",
+                   region))
+    }
+  } else {
+    match_value(region, sircovid::regions("all"))
+  }
+  region
+}
